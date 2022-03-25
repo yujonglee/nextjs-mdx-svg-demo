b220d5298368f054e8cb848f44b2e5b078bdf1e9
yujong-lee <yujonglee@g.skku.edu>
2022-03-25 19:56:37 +0900

    Add configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx', 'mdx'],
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.mdx$/,
+        use: [{ loader: '@mdx-js/loader' }]
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"]
+      }
+    );
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
---
+++ pages/MyComponent.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const MyComponent: NextPage = () => {
+    return (
+      <div>Hello World</div>
+  )
+}
+
+export default MyComponent
